83913ea31090be9b985bae06f5d5153ef57a78df
Guille LA <guillermolucena90@gmail.com>
2019-06-27 20:25:08 +0200

    added routing to landings

--- sharingweb-web/src/components/App.js
+++ sharingweb-web/src/components/App.js
@@ -2,14 +2,42 @@ import React from 'react';
 import '../css/App.css';
 import {Route} from 'react-router-dom'
 import Home from '../scenes/home/Home'
+import ShopService from './services/ShopService'
 
 
-function App() {
-  return (
-    <div>
-      <Route exact path="/" component={Home}></Route>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    shops: false
+  }
+  
+  componentDidMount() {
+    ShopService.listShops()
+      .then(shops => {this.setState({
+        shops: shops.data
+      })},
+      error => console.error(error))
+  }
+
+
+  render() {
+    const {shops} = this.state
+    console.log(shops)
+    let list;
+    if (shops){
+    list = shops.map( (item, i) => {
+      return (
+        <Route key={i} exact path={`/shops/${item.name}`} render={() => <div>hola</div>}></Route>
+      )})
+    }
+    
+
+    return (
+      <div>
+        <Route exact path="/" component={Home}></Route>
+        { shops && list}
+      </div>
+    );
+  }
 }
 
 export default App;
