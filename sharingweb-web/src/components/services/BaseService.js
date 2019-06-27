f835f560edb6d02426f4610e05dd39b300c6e292
Guille LA <guillermolucena90@gmail.com>
2019-06-27 17:42:56 +0200

    added services

---
+++ sharingweb-web/src/components/services/BaseService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+
+const http = axios.create({
+  baseURL: `http://localhost:3001`,
+  withCredentials: true
+})
+
+http.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response.status === 403 || error.response.status === 401) {
+      window.location.assign("/auth/login");
+    }
+    else {
+      return Promise.reject(error);
+    }
+  }
+)
+
+export default http;
