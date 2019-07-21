6cc4833915143657aca9372ede2b296449aa36cd
Guille LA <guillermolucena90@gmail.com>
2019-07-21 21:09:04 +0200

    corrected carrusel

--- sharingweb-web/src/scenes/home/components/Examples.js
+++ sharingweb-web/src/scenes/home/components/Examples.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import "../css/example.css"
-import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from
+import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer } from
 "mdbreact";
+import {Redirect} from 'react-router-dom'
+import {withShopContext} from '../../../contexts/ShopStore'
 
+class Example extends React.Component {
+  state = {
+    goToLolo: false,
+    goToSauri: false,
+    goToShirts: false
+  }
 
-const Example = () => {
+  handleclickLolo = () => {
+    this.props.updateShop("fruteria-manolo")
+    this.setState({
+      goToLolo: true
+    })
+  }
+  handleclickSauri = () => {
+    this.props.updateShop("confecciones-sauri")
+    this.setState({
+      goToSauri: true
+    })
+  }
+  handleclickShirts = () => {
+    this.props.updateShop("trishirts")
+    this.setState({
+      goToShirts: true
+    })
+  }
 
-  return(
+  render() {
+    const {goToLolo, goToSauri, goToShirts} = this.state
+  return (
+    <React.Fragment>
+      {goToLolo && <Redirect to="/shops/fruteria-manolo"></Redirect>}
+      {goToSauri && <Redirect to="/shops/confecciones-sauri"></Redirect>}
+      {goToShirts && <Redirect to="/shops/trishirts"></Redirect>}
     <div data-aos="fade-up"
     data-aos-offset="200"
     data-aos-delay="20"
@@ -15,50 +46,51 @@ const Example = () => {
     data-aos-mirror="true"
     data-aos-once="false"
     data-aos-anchor-placement="top" className="example-section">
-      <MDBContainer id="examples">
-        <MDBCarousel activeItem={1} length={3} showControls={true} showIndicators={true} className="z-depth-1">
+    <MDBContainer>
+      <MDBCarousel
+        activeItem={1}
+        length={3}
+        showControls={true}
+        showIndicators={false}
+        className="z-depth-1"
+      >
         <MDBCarouselInner>
           <MDBCarouselItem itemId="1">
-            <MDBView><img className="d-block w-100" src="https://josefacchin.com/wp-content/uploads/2016/02/landing-page-wordpress.png" alt="First slide"/>
-            <MDBMask overlay="black-light" />
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="https://res.cloudinary.com/dvjtdeyli/image/upload/v1563735005/game-images/fruteriaManolo_mkgalv.png"
+                alt="First slide"
+                onClick={() => this.handleclickLolo()}
+              />
             </MDBView>
-            <MDBCarouselCaption>
-            </MDBCarouselCaption>
           </MDBCarouselItem>
           <MDBCarouselItem itemId="2">
             <MDBView>
               <img
                 className="d-block w-100"
-                src="https://s3.amazonaws.com/cdn.wp.m4ecnet/wp-content/uploads/2018/12/04104047/atribuci%C3%B3n-compras-online-compressor.jpg"
+                src="https://res.cloudinary.com/dvjtdeyli/image/upload/v1563734999/game-images/sauri_htcdjp.png"
                 alt="Second slide"
+                onClick={() => this.handleclickSauri()}
               />
-            <MDBMask overlay="black-strong" />
             </MDBView>
-            <MDBCarouselCaption>
-              <h3 className="h3-responsive">Shop online</h3>
-              <p></p>
-            </MDBCarouselCaption>
           </MDBCarouselItem>
           <MDBCarouselItem itemId="3">
             <MDBView>
               <img
                 className="d-block w-100"
-                src="https://www.abtasty.com/content/uploads/landing-page-conversion.png"
+                src="https://res.cloudinary.com/dvjtdeyli/image/upload/v1563734994/game-images/trishirts_s1xqcw.png"
                 alt="Third slide"
+                onClick={() => this.handleclickShirts()}
               />
-            <MDBMask overlay="black-slight" />
             </MDBView>
-            <MDBCarouselCaption>
-              <h3 className="h3-responsive">Accesibilidad</h3>
-              <p></p>
-            </MDBCarouselCaption>
           </MDBCarouselItem>
         </MDBCarouselInner>
       </MDBCarousel>
-      </MDBContainer>
+    </MDBContainer>
     </div>
-    
-  )
+    </React.Fragment>
+  );}
 }
 
-export default Example
+export default withShopContext(Example);
